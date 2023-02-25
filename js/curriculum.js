3059ab5fc5cd0ae69a49197e636509795919aee4
Damuchi99 <damuchi99@gmail.com>
2023-02-25 02:59:41 -0300

    HTML-CSS-JS: Página sobre un curriculum vitae. Primer commit.

---
+++ js/curriculum.js
@@ -0,0 +1,20 @@
+var menu_visible = false;
+let menu = document.getElementById("menu");
+function mostrarOcultarMenu(){
+    if(menu_visible==false){
+        menu.style.display = "block";
+        menu_visible = true;
+    }else{
+        menu.style.display = "none"
+        menu_visible = false;
+    }
+}
+
+let links = document.querySelectorAll("nav a");
+
+for(var i=0; i<links.length; i++){
+    links[i].onclick = function(){
+        menu.style.display = "none"
+        menu_visible = false;
+    }
+}
